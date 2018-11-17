8587a65a97154f9a677bd5f876801f1eb3c0371a
Matt Nierzwicki <matt.nierzwicki@gmail.com>
2018-11-16 19:47:17 -0500

    Allow store authentication via oauth for Github and Facebook

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import firebase from "firebase";
 
 import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
+import Login from "./Login";
+import base, { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
   static propTypes = {
@@ -13,10 +16,57 @@ class Inventory extends React.Component {
     fishes: PropTypes.object.isRequired
   };
 
-  render() {
+  state = {
+    uid: null,
+    owner: null
+  };
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.authHandler({ user });
+      }
+    });
+  }
+
+  authHandler = async authData => {
+    // 1. Look up current store in firebase
+    const store = await base.fetch(this.props.storeId, { context: this });
+
+    // 2. Claim store if there's no current owner
+    if (!store.owner) {
+      await base.post(`${this.props.storeId}/owner`, {
+        data: authData.user.uid
+      });
+    }
+
+    // 3. Set the state of the inventory component to reflect the current user
+    this.setState({
+      uid: authData.user.uid,
+      owner: store.owner || authData.user.uid
+    });
+  };
+
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  };
+
+  logout = async () => {
+    await firebase.auth().signOut();
+    this.setState({
+      uid: null
+    });
+  };
+
+  renderEditInventory() {
     return (
       <div className="inventory">
         <h2>Inventory</h2>
+        <button onClick={this.logout}>Log out</button>
         {Object.keys(this.props.fishes).map(key => (
           <EditFishForm
             key={key}
@@ -30,6 +80,28 @@ class Inventory extends React.Component {
         <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
       </div>
     );
+  }
+
+  render() {
+    const logout = <button onClick={this.logout}>Log out</button>;
+
+    // Check if user is logged in
+    if (!this.state.uid) {
+      return <Login authenticate={this.authenticate} />;
+    }
+
+    // Check if logged in user is not the owner of the store
+    if (this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>Sorry you are not the owner!</p>
+          {logout}
+        </div>
+      );
+    }
+
+    // Logged in user is the owner, render the inventory
+    return this.renderEditInventory();
   }
 }
 
